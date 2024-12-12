b56e6d4e656a1a6a3b602cc24092e86fed46ab1a
keitti73 <keitti73@gmail.com>
2024-12-12 10:27:42 +0900

    依存関係のndarrayとndarray-randのバージョンを更新し、サンプルデータ生成のロジックを改善。行列の合計値計算をクロージャを使用して実装。

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,42 @@
 use field_element::FieldElement;
 use rand::distributions::*;
-use ndarray::*;
+use ndarray::prelude::*;
 use ndarray_rand::RandomExt;
-use ndarray_linalg::solve::Inverse;
+use ndarray_linalg::{Solve, Inverse};
 
 fn main() {
-    let mut sum_encoded_sample:Vec<FieldElement<i32>> = Vec::new();
-
+    let prime = (2_u32.pow(31) - 1) as i128;
+    let mut sum_matrix: Vec<FieldElement<i128>>= Vec::new();
+    let mut random_matrix_1: Vec<Array1<FieldElement<i128>>> = Vec::new();
     // ランダムなサンプルデータを生成
-    let sample: Array1<FieldElement<i32>> = Array::random(5, Uniform::new(0, 255)).mapv(|x| FieldElement::new(x, 256));
+    let sample: Array1<FieldElement<i128>> = Array::random(5, Uniform::new(0, 255)).mapv(|x| FieldElement::new(x, prime));
     println!("Original sample: {:?}", sample);
 
-    // ランダムな行列を生成
-    let random_matrix: Array2<i32> = Array::random((1, 5), Uniform::new(0, 255)).mapv(|x| x);
-    println!("Random matrix: {:?}", random_matrix);
-    // 符号化
-    let encoded_sample = &sample*&random_matrix;
-    println!("Encoded sample: {:?}", encoded_sample);
+    for i in 0..5 {
+        let random_matrix: Array1<FieldElement<i128>> = Array::random(5, Uniform::new(0, 255)).mapv(|x| FieldElement::new(x, prime));
+        //println!("Random matrix: {:?}", &random_matrix);
+        random_matrix_1.push(random_matrix.clone());
 
-     // 合計値を計算
-    let sum: FieldElement<i32> = encoded_sample.iter().fold(FieldElement::new(0, 256), |acc, x| acc + x.clone());
-    println!("Sum of encoded sample: {:?}", sum);
-    sum_encoded_sample.push(sum);
+        // 符号化
+        let encoded_sample = &sample * &random_matrix;
+        //println!("Encoded sample: {:?}", encoded_sample);
+
+        // 合計値を計算するクロージャ
+        let sum_closure = |arr: &Array1<FieldElement<i128>>| -> FieldElement<i128> {
+            arr.iter().fold(FieldElement::new(0, prime), |acc, x| acc + x.clone())
+        };
+
+        // 合計値を計算
+        let sum = sum_closure(&encoded_sample);
+        //println!("Sum of encoded sample: {:?}", &sum);
+        sum_matrix.push(sum)
+    }
+
+    let sum_matrix = Array1::from_vec(sum_matrix).t() ;
+    let random_matrix = Array::from_shape_vec((5, 5),random_matrix_1.into_iter().flatten().collect()).unwrap();
+    println!("random_matrix_1: {:?}", random_matrix);
+    let random_matrix_inv = random_matrix.inv().expect("Matrix inversion failed");
+    let x_vec = random_matrix_inv.dot(&sum_matrix);
 }
 // https://github.com/shinbunbun/secp256k1-rust
-// field_elementは上記のコードの一部をお借りしました
+// field_elementは上記のコードの一部をお借りしま・・
