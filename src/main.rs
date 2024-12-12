2d77a4520cc9c0305344ca926a4e315cda74475d
keitti73 <keitti73@gmail.com>
2024-12-12 11:40:31 +0900

    依存関係のバージョンを更新し、行列計算のロジックを改善。ガウス消去法を実装してサンプルデータのデコードを行う機能を追加

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,21 @@ use field_element::FieldElement;
 use rand::distributions::*;
 use ndarray::prelude::*;
 use ndarray_rand::RandomExt;
-use ndarray_linalg::{Solve, Inverse};
 
 fn main() {
     let prime = (2_u32.pow(31) - 1) as i128;
-    let mut sum_matrix: Vec<FieldElement<i128>>= Vec::new();
-    let mut random_matrix_1: Vec<Array1<FieldElement<i128>>> = Vec::new();
+    let mut sum_matrix: Vec<FieldElement<i128>> = Vec::new();
+    let mut random_matrix_1: Vec<Vec<FieldElement<i128>>> = Vec::new();
     // ランダムなサンプルデータを生成
-    let sample: Array1<FieldElement<i128>> = Array::random(5, Uniform::new(0, 255)).mapv(|x| FieldElement::new(x, prime));
-    println!("Original sample: {:?}", sample);
+    let sample: Array1<FieldElement<i128>> = Array::random(5, Uniform::new(0, prime - 1)).mapv(|x| FieldElement::new(x, prime));
+    //println!("Original sample: {:?}", sample);
 
-    for i in 0..5 {
+    for _ in 0..5 {
         let random_matrix: Array1<FieldElement<i128>> = Array::random(5, Uniform::new(0, 255)).mapv(|x| FieldElement::new(x, prime));
-        //println!("Random matrix: {:?}", &random_matrix);
-        random_matrix_1.push(random_matrix.clone());
+        random_matrix_1.push(random_matrix.to_vec());
 
         // 符号化
         let encoded_sample = &sample * &random_matrix;
-        //println!("Encoded sample: {:?}", encoded_sample);
 
         // 合計値を計算するクロージャ
         let sum_closure = |arr: &Array1<FieldElement<i128>>| -> FieldElement<i128> {
@@ -28,17 +25,54 @@ fn main() {
 
         // 合計値を計算
         let sum = sum_closure(&encoded_sample);
-        //println!("Sum of encoded sample: {:?}", &sum);
         sum_matrix.push(sum)
     }
 
-    let sum_matrix = Array1::from_vec(sum_matrix).t() ;
-    let random_matrix = Array::from_shape_vec((5, 5),random_matrix_1.into_iter().flatten().collect()).unwrap();
-    println!("random_matrix_1: {:?}", random_matrix);
-    let random_matrix_inv = random_matrix.inv().expect("Matrix inversion failed");
-    let x_vec = random_matrix_inv.dot(&sum_matrix);
+    let sum_matrix = Array::from(sum_matrix).to_owned();
+    let random_matrix: Array2<FieldElement<i128>> = Array2::from_shape_vec((5, 5), random_matrix_1.iter().flatten().cloned().collect()).unwrap();
+    //println!("Random matrix: {:?}", random_matrix);
+    let x_vec = gaussian_elimination(&random_matrix, &sum_matrix);
+    //println!("Decoded sample: {:?}", x_vec);
 
-    println!("Decoded sample: {:?}", x_vec);
+    println!("{:?}", sample==x_vec);
 }
-// https://github.com/shinbunbun/secp256k1-rust
-// field_elementは上記のコードの一部をお借りしま・・
+
+fn gaussian_elimination(a: &Array2<FieldElement<i128>>, b: &Array1<FieldElement<i128>>) -> Array1<FieldElement<i128>> {
+    let n = a.nrows();
+    let mut a = a.clone();
+    let mut b = b.clone();
+
+    for i in 0..n {
+        let mut max_row = i;
+        for k in (i + 1)..n {
+            if a[[k, i]].num.abs() > a[[max_row, i]].num.abs() {
+                max_row = k;
+            }
+        }
+        // 手動で行を入れ替える
+        for j in 0..a.ncols() {
+            a.swap([i, j], [max_row, j]);
+        }
+        b.swap(i, max_row);
+
+        for k in (i + 1)..n {
+            let factor = a[[k, i]].clone() / a[[i, i]].clone();
+            for j in i..n {
+                a[[k, j]] = a[[k, j]].clone() - factor.clone() * a[[i, j]].clone();
+            }
+            b[k] = b[k].clone() - factor * b[i].clone();
+        }
+    }
+
+    let mut x: Array1<FieldElement<i128>> = Array1::from_elem(n, FieldElement::new(0, a[[0, 0]].prime));
+    for i in (0..n).rev() {
+        let mut sum = FieldElement::new(0, a[[i, i]].prime);
+        for j in (i + 1)..n {
+            sum = sum + a[[i, j]].clone() * x[j].clone();
+        }
+        x[i] = (b[i].clone() - sum) / a[[i, i]].clone();
+    }
+
+    x
+}
+
