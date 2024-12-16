b99f3e9d03366eb1e36bd8c5edd65c01a16b57f4
keitti73 <keitti73@gmail.com>
2024-12-16 14:46:21 +0000

    .gitignoreにout.rsを追加し、main.rsのエラーハンドリングを改善。Sample構造体を修正し、random_sample関数をコメントアウト。出力をファイルに書き込む機能を追加。

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use field_element::FieldElement;
-use rand::distributions::*;
+//use rand::distributions::*;
 use ndarray::prelude::*;
-use ndarray_rand::RandomExt;
+//use ndarray_rand::RandomExt;
 mod encoder;
 mod decoder;
 
@@ -9,17 +9,18 @@ use std::fmt::Debug;
 use std::ops::Sub;
 
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read, Write};
 
-struct Sample<T>{
+struct Sample<T,U>{
     value: Vec<T>,
-    prime: T,
+    prime: U,
 }
-impl<T> Sample<T>
+impl<T,U> Sample<T,U>
 where
     T: Clone + Debug + PartialOrd + From<u32> + Sub<Output = T> + Into<i128>,
+    U: Clone + Debug + Into<i128>,
 {
-    fn get_value(&self) -> Vec<FieldElement<i128>> {
+    fn to_value(&self) -> Vec<FieldElement<i128>> {
         let prime: i128 = self.prime.clone().into();
         self.value
             .clone()
@@ -27,23 +28,24 @@ where
             .map(|x| FieldElement::new((*x).clone().into(), prime))
             .collect()
     }
+
     fn get_prime(&self) -> i128 {
         self.prime.clone().into()
     }
 
-    fn random_sample() -> Sample<u32> {
-        let prime: u32 = 2_u32.pow(31) - 1;
-        let shape:u8 = rand::random();
-        let value = Array::random(shape as usize , Uniform::new(0,prime-1)).to_vec();
-        Sample { value, prime }
-    }
+    //fn random_sample() -> Sample<u32,u32> {
+    //    let prime: u32 = 2_u32.pow(31) - 1;
+    //    let shape:u8 = rand::random();
+    //    let value = Array::random(shape as usize , Uniform::new(0,prime-1)).to_vec();
+    //    Sample { value, prime }
+    //}
 }
 
 
-fn main() {
-    let mut file = File::open("./src/main.rs").unwrap();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = File::open("./src/main.rs")?;
     let mut buf: Vec<u8> = Vec::new();
-    let _ = file.read_to_end(&mut buf);
+    let _ = file.read_to_end(&mut buf)?;
 
     let u32_buf: Vec<u32> = buf.chunks_exact(4)
         .map(|chunk| {
@@ -55,10 +57,10 @@ fn main() {
 
     //println!("{:?}", u32_buf);
 
-    let sample_data: Sample<u32> = Sample { value: u32_buf, prime:  2_u32.pow(31) - 1 };
+    let sample_data: Sample<u32,u32> = Sample { value: u32_buf.clone(), prime:  2_u32.pow(31) - 1 };
 
     let prime: i128 = sample_data.get_prime();
-    let sample: Array1<FieldElement<i128>> = sample_data.get_value().into();
+    let sample: Array1<FieldElement<i128>> = sample_data.to_value().into();
 
     let mut sum_matrix: Vec<FieldElement<i128>> = Vec::new();
     let mut random_matrix_tmp: Vec<Vec<FieldElement<i128>>> = Vec::new();
@@ -74,6 +76,15 @@ fn main() {
 
     println!("{:?}", sample==x_vec);
     println!("{:?}", sample.len());
+
+
+    let x_vec_u32: Vec<u32> = x_vec.into_iter().map(|x| x.num as u32).collect();
+    println!("{}", u32_buf == x_vec_u32);
+
+    let mut file = File::create("out.rs")?;
+    for num in x_vec_u32 {
+        file.write_all(&num.to_le_bytes())?;
+    };
+    buf.flush()?;
+    Ok(())
 }
-
-
